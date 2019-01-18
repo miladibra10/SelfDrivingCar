f60b480dbf3879cc2ca2eccdc3d801103f934f42
Milad <miladibra10@gmail.com>
2019-01-18 07:32:47 +0330

    driver controller completed

--- drive.py
+++ drive.py
@@ -1,37 +1,70 @@
 import argparse
+import base64
 import os
 import shutil
+from datetime import datetime
+from io import BytesIO
+from keras.models import load_model
+import utils
 import eventlet.wsgi
+import numpy
 import socketio
+from PIL import Image
 from flask import Flask
 
 socket = socketio.Server()
 app = Flask(__name__)
 model = None
 image_list = None
-
 MAX_SPEED = 30
 MIN_SPEED = 10
-
 speed_limit = MAX_SPEED
 
 
 class Sender:
     def __init__(self):
-        self.image_save = True
+        self.image_save = False
 
     def send(self, angle, throttle):
         socket.emit("steer", data={'steering_angle': str(angle), 'throttle': str(throttle)}, skip_sid=True)
 
 
+sender = Sender()
+
+
 @socket.on('telemetry')
 def telemetry(sid, data):
-    print("Data Sent to Driver Controller from Simulator")
+    if data:
+        speed = float(data["speed"])
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        if sender.image_save:
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+
+        try:
+            image = numpy.asarray(image)
+            image = utils.preprocess(image)
+            image = numpy.array([image])
+            steering_angle = float(model.predict(image, batch_size=1))
+            global speed_limit
+            if speed > speed_limit:
+                speed_limit = MIN_SPEED
+            else:
+                speed_limit = MAX_SPEED
+            throttle = 1.0 - steering_angle ** 2 - (speed / speed_limit) ** 2
+            sender.send(steering_angle, throttle)
+        except Exception as e:
+            print(e)
+
+    else:
+        socket.emit('manual', data={}, skip_sid=True)
 
 
 @socket.on('connect')
 def connect(sid, environ):
     print("Connection with Simulator Established")
+    sender.send(0, 0)
 
 
 if __name__ == '__main__':
@@ -40,9 +73,10 @@ if __name__ == '__main__':
     parser.add_argument('image_folder', type=str, nargs='?', default='', help='Images for this run will be saved in this path')
     args = parser.parse_args()
 
-    # TODO loading model
+    model = load_model(args.model)
 
     if args.image_folder != '':
+        sender.image_save = True
         print("Creating image folder at {}".format(args.image_folder))
         if not os.path.exists(args.image_folder):
             os.makedirs(args.image_folder)
